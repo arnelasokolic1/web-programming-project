a0b006a68b258d4673315c056dcf03199233172e
arnelasokolic1 <arnela.sokolic1@gmail.com>
2024-05-14 00:13:04 +0200

    Milestone4

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+
 require 'vendor/autoload.php';
-require 'rest/routes/routes.php';
-?>
+require 'rest/routes/actions_routes.php';
+require 'rest/routes/admin_routes.php';
+require 'rest/routes/customer_routes.php';
+require 'rest/routes/messages_routes.php';
+require 'rest/routes/products_routes.php';
+
+Flight::start();
